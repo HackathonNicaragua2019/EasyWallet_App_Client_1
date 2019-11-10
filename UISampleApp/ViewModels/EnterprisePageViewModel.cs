d3150c4175e54623c8b636d3e05e3a64a75c851b
real-fidel1999@hotmail.com <real-fidel1999@hotmail.com>
2019-11-10 09:53:46 -0600

    ajustes de modelos y las interfaces

---
+++ UISampleApp/ViewModels/EnterprisePageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using UISampleApp.Models;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace UISampleApp.ViewModels
+{
+   
+
+    public class EnterprisePageViewModel : NotificationObject
+    {
+        private string lorem = "Su tienda de conveniencia mas surtida";
+
+        private ObservableCollection<Empresa> empresas;
+
+        public ObservableCollection<Empresa> Empresas
+        {
+            get { return empresas; }
+            set { empresas = value;
+                onPropertyChanged();
+            }
+        }
+
+        private Empresa selectedEnterprise;
+
+        public Empresa SelectedEnterprise
+        {
+            get { return selectedEnterprise; }
+            set { selectedEnterprise = value;
+                onPropertyChanged();
+            }
+        }
+
+
+        public EnterprisePageViewModel(string categoria)
+        {
+            //Aqui se supone que vendria una query bien vergas,claro,si hubiera BD
+            //Y api terminada
+
+            Empresas = new ObservableCollection<Empresa>();
+
+            Empresas.Add(new Empresa() { Estrellas=4.5,FechaAfiliacion = DateTime.Now, Ilustracion= "isoBuy.png",Nombre="Super express",Descripcion=lorem});
+            Empresas.Add(new Empresa() { Estrellas=3.5,FechaAfiliacion = DateTime.Now, Ilustracion= "isoBuy.png", Nombre="Ferreteria jenny",Descripcion="Las herramientas que desee al mejor precio"});
+            Empresas.Add(new Empresa() { Estrellas=2.5,FechaAfiliacion = DateTime.Now, Ilustracion= "isoBuy.png", Nombre="McDonald",Descripcion=lorem});
+            Empresas.Add(new Empresa() { Estrellas=5.0,FechaAfiliacion = DateTime.Now, Ilustracion= "isoBuy.png", Nombre="GoDealer",Descripcion="Tu dealer 24/7,discrecion en nuestra operacion"});
+            Empresas.Add(new Empresa() { Estrellas=1.5,FechaAfiliacion = DateTime.Now, Ilustracion= "isoBuy.png", Nombre="DevSocket",Descripcion="Las mejores soluciones de software al mejor precio"});
+
+            PropertyChanged += EnterprisePageViewModel_PropertyChanged;
+        }
+
+        private void EnterprisePageViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(this.SelectedEnterprise))
+            {
+                //Enviar mensaje para navegar
+                MessagingCenter.Send(this,"GotoInventario");
+            }
+        }
+    }
+}
---
+++ UISampleApp/Views/Enterprises/EnterprisePage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UISampleApp.Models;
+using UISampleApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UISampleApp.Views.Enterprises
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnterprisePage : ContentPage , INotifyPropertyChanged
+    {
+        private EnterprisePageViewModel context;
+
+        public EnterprisePageViewModel Context
+        {
+            get { return context; }
+            set { context = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public EnterprisePage(string category)
+        {
+            //Context 
+             Context = new EnterprisePageViewModel(category);
+
+            //Crear recurso en base a la categoria enviada
+
+
+            InitializeComponent();
+
+            lstEnterprises.BindingContext = Context;
+
+
+            MessagingCenter.Subscribe<EnterprisePageViewModel>(this, "GotoInventario", (x) => {
+                //Respuesta
+                gotoInventario(((Empresa)lstEnterprises.SelectedItem).Nombre);
+
+            });
+
+        }
+
+        private void SldStars_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private async void gotoInventario(string empresa)
+        {
+            await Xamarin.Forms.Application.Current.MainPage.Navigation.PushModalAsync(new UISampleApp.Views.Enterprises.InventarioPage(empresa));
+        }
+
+    }
+}
